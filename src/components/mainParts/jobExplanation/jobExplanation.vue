c55597c67525cac38ec36051af61ad40f89cbd78
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-11-20 18:46:38 +0330

    explanation logo and texts added

--- src/components/mainParts/jobExplanation/jobExplanation.vue
+++ src/components/mainParts/jobExplanation/jobExplanation.vue
@@ -1,13 +1,24 @@
 <template>
-  <h1>jobExplanation</h1>
+  <section class="Explanation px-12 py-12">
+    <span class="ExplanationTitle__span text-[30px]">Front-End Journey</span>
+    <p class="Explanation__p text-[15px] max-w-[60%] mb-10">
+      A front-end developer builds the front-end portion of websites and web applications—the part
+      users see and interact with.<br />
+      A front-end developer is a type of software developer who creates the user-facing side of
+      websites and applications. They are well-versed in web development languages such as HTML,
+      CSS, and JavaScript that are designed for coding dynamic, interactive user experiences.
+      Anytime you enjoy the look and feel of a digital product, website, or application, you have a
+      front-end developer to thank.
+    </p>
+    <a href="https://www.coursera.org/articles/front-end-developer" target="_blank"  class="Explanation__a  px-5 text-[15px]">Read More</a>
+  </section>
 </template>
 
 <script>
 export default {
-name:"jobExplanation"
+  name: 'jobExplanation'
 }
 </script>
 
 <style>
-
 </style>
