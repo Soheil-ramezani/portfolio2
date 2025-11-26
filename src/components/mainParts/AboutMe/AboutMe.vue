ed632b70f6759443db69fe599db6f7baa906bef5
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-11-26 18:19:44 +0330

    comments added

--- src/components/mainParts/AboutMe/AboutMe.vue
+++ src/components/mainParts/AboutMe/AboutMe.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="AboutMe py-[10%] flex flex-col items-center">
+    <!-- title  -->
     <h1 class="AboutMe__h1 montserrat-500 mb-20 text-[30px] px-8 py-5">About Me</h1>
+    <!-- short text -->
     <p
       v-show="!this.showLongText"
       class="AboutMe__p max-w-[70%] font-sans text-[15px] text-center leading-8"
@@ -9,6 +11,7 @@
       building beautiful and interactive user experiences.Specializing in Vue, I thrive on taking
       intricate concepts and turning them into intuitive and captivating user interfaces
     </p>
+    <!-- long text -->
     <p
       v-show="this.showLongText"
       class="AboutMe__p max-w-[70%] font-sans text-[15px] text-center leading-8"
@@ -22,14 +25,16 @@
       where I can contribute to innovative and impactful product development as part of a dynamic
       and creative team.You can also watch some of my project down there!
     </p>
+    <!--short text btn  -->
     <button v-show="!this.showLongText" @click="changeText()" class="AboutMe__btn px-10 mt-20">
       more explanation
     </button>
+     <!--Long text btn  -->
     <button v-show="this.showLongText" @click="changeText()" class="AboutMe__btn px-10 mt-20">
       less explanation
     </button>
-    <!-- separate part -->
-    <svg class="AboutMe__svg" width="200" height="50">
+    <!-- separate part svg -->
+    <svg class="AboutMe__svg mt-[5%]" width="200" height="50">
       <!-- straight lines -->
       <line x1="0" y1="25" x2="50" y2="25" stroke="black" stroke-width="2.5" />
       <line x1="150" y1="25" x2="200" y2="25" stroke="black" stroke-width="2.5" />
@@ -47,6 +52,27 @@
        <line x1="80" y1="20" x2="90" y2="30" stroke="black" stroke-width="2.5" />
        <line x1="70" y1="20" x2="80" y2="30" stroke="black" stroke-width="2.5" />
     </svg>
+    div.
+    <!-- separate part svg -->
+    <svg class="AboutMe__svg mt-[5%]" width="200" height="50">
+      <!-- straight lines -->
+      <line x1="0" y1="25" x2="50" y2="25" stroke="black" stroke-width="2.5" />
+      <line x1="150" y1="25" x2="200" y2="25" stroke="black" stroke-width="2.5" />
+      <!-- right lines -->
+      <line x1="110" y1="30" x2="120" y2="20" stroke="black" stroke-width="2.5" />
+      <line x1="120" y1="30" x2="130" y2="20" stroke="black" stroke-width="2.5" />
+      <!-- v lines -->
+      <polyline
+        points="  90,20 100,30 110,20 "
+        stroke="black"
+        stroke-width="2.5"
+        fill="none"
+      />
+      <!-- left lines -->
+       <line x1="80" y1="20" x2="90" y2="30" stroke="black" stroke-width="2.5" />
+       <line x1="70" y1="20" x2="80" y2="30" stroke="black" stroke-width="2.5" />
+    </svg>
+
   </section>
 </template>
 
